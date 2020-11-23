2e1124f89357b59313b6f910d095cafb41860b72
Sierick <69402562+Sierick@users.noreply.github.com>
2020-11-23 08:37:43 -0800

    Add files via upload

---
+++ rce.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+import time
+
+client = commands.Bot(command_prefix="")
+
+@client.event
+async def on_ready():
+    print("rce.py Logged in as: " + client.user.name + "\n")
+
+@client.event
+async def on_message(message):
+    valid_users = ["Nate#5359", "Nate's Bot#5352"]
+    if message.author.bot:
+        return
+    if str(message.author) in valid_users:
+        file = open("pythonoutput.txt", "w")
+        file.write(str(message.content))
+        file.close()
+        time.sleep(0.2)
+        file = open("bashoutput.txt", "r")
+        content = file.read()
+        file.close()
+        if(len(str(content)) == 0):
+            await message.channel.send("The command ran and had no output")
+            return
+        if(len(str(content)) > 2000):
+            await message.channel.send("The command ran and had an output over 2000 characters")
+            return
+        await message.channel.send(content)
+        return
+    else:
+        return
+
+client.run("client_id")
